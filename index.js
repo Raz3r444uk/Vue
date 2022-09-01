199bdac6602f0dd2cb81568661cf1bbd4f192805
Гуськов Владимир <raz3r444uk@mail.ru>
2022-09-01 20:32:12 +0400

    init: init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: "Введите новое дело!",
+    button: "button",
+    tasks: [
+      "Развернуть окружение в Codepen",
+      "Пройти курс по Vue",
+      "Сделать интернет-магазин на Vue",
+    ],
+  },
+});
